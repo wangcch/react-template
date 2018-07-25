235e7e9b905b861130f53715a1006e6835913499
wangcch <ci713@qq.com>
2018-07-25 16:26:44 +0800

    update: router and no-match

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact component={NoMatch} />
+      </Switch>
     );
   }
 }
