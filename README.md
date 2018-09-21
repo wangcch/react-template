325842813ef174ca17c526408a76125c7ef7c74f
wangcch <ci713@qq.com>
2018-09-21 15:01:14 +0800

    fix(config-overrides): webpack resolve conf

--- README.md
+++ README.md
@@ -44,11 +44,11 @@ const rewireSass = require("react-app-rewire-scss");
 
 module.exports = function override(config, env) {
   config = rewireSass(config, env);
-  config.resolve = {
+  config.resolve = Object.assign({}, config.resolve, {
     alias: {
       "@": path.resolve("src")
     }
-  };
+  });
   return config;
 };
 ```
--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 const rewireSass = require("react-app-rewire-scss");
 
 module.exports = function override(config, env) {
   config = rewireSass(config, env);
-  config.resolve = {
+  config.resolve = Object.assign({}, config.resolve, {
     alias: {
       "@": path.resolve("src")
     }
-  };
+  });
   return config;
 };
